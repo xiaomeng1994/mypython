55a1b83404256c22cb462e0f35eb6babc754a3c0
xiaomeng1994 <39402620+xiaomeng1994@users.noreply.github.com>
2018-07-20 16:05:44 +0800

    Update excelToMysql.py

--- excelToMysql.py
+++ excelToMysql.py
@@ -19,27 +19,7 @@ def chinese_to_pinyin(arr):
 
 
 if __name__ == '__main__':
-    file_list = [
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/19.遣送出境外国人情况--国籍前五位.xlsx',1,'hqwsw_qscjwgrqk_gjqww'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/18.遣送出境外国人情况--按市辖区分.xlsx',1,'hqwsw_qscjwgrqk_asxqf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/17.查处“三非”外国人情况--按违法事项分.xlsx',1,'hqwsw_ccsfwgrqk_awfsxf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/16.查处“三非”外国人情况--国籍前五位.xlsx',1,'hqwsw_ccsfwgrqk_gjqww'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/15.查处“三非”外国人情况--按市辖区分.xlsx',1,'hqwsw_ccsfwgrqk_asxqf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/14.实有外国人情况--国籍前五位.xlsx',1,'hqwsw_sywgrqk_gjqww'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/13.实有外国人情况--按大洲分.xlsx',1,'hqwsw_sywgrqk_adzf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/12.实有外国人情况--按市辖区分.xlsx',1,'hqwsw_sywgrqk_asxqf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/11.住宿登记情况--国籍前五位.xlsx',1,'hqwsw_zsdjqk_gjqww'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/10.住宿登记情况--按大洲分.xlsx',1,'hqwsw_zsdjqk_adzf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/9.住宿登记情况--按市辖区分.xlsx',1,'hqwsw_zsdjqk_asxqf'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/8.刑拘越南人情况统计表.xlsx',1,'hqwsw_xjynrqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/7.中小学在读外国学生情况统计表.xlsx',1,'hqwsw_zxxzdwgxsqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/6.小孩出生情况统计表.xlsx',1,'hqwsw_xhcsqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/5.离婚登记情况统计表.xlsx',1,'hqwsw_lhdjqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/4.结婚登记情况统计表.xlsx',1,'hqwsw_jhdjqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/3.传染病情况统计表.xlsx',1,'hqwsw_crbqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/2.报列不准入境情况统计表.xlsx',1,'hqwsw_blbzrjqk'),
-        ('C:/Users/1/Desktop/广州人大/整理/文档管理/智慧人大/8_涉外管理联网监督系统/清洗后的数据/1.入境外国人情况统计表.xlsx',1,'hqwsw_rjwgrqk')]
-
+    file_list = [('C:/test.xlsx',1,'test')]
     index = 15
     # for index in  range(file_list.__len__()):
     print(index)
